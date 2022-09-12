015e83ed847ec4ef8c38361cf3c0369236021b23
Judith <judithchard1111@gmail.com>
2022-09-12 16:37:48 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ *
+ * Return: always return 0
+ *
+ */
+int main(void)
+{
+	char a;
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'q' || a == 'e')
+		{
+			continue;
+		}
+		printf("%c", a);
+	}
+	printf("\n");
+	return (0);
+}
