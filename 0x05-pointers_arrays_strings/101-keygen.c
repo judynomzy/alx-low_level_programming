9fd1012586c5c3d091d877a417b4d43ead02f9f7
Judith <judithchard1111@gmail.com>
2022-09-19 22:05:09 -0700

    PASSWD

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,133 @@
+#include <math.h>
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+
+
+/**
+ *
+ *  * main - entry point
+ *
+ *   *
+ *
+ *    * key_gen - password generator
+ *
+ *     *
+ *
+ *      * return: Always zero(success)
+ *
+ *       *
+ *
+ *        */
+
+
+
+int key_gen(void)
+
+{
+
+	int i;
+
+	int r;
+
+
+
+	char numbers[] = "0123456789";
+
+	char letter[] = "abcdefghijklmnoqprstuvwyzx";
+
+	char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+
+	char symbols[] = "!@#$^&*?";
+
+	char password[12];
+
+
+
+	srand((unsigned int)(time(NULL)));
+
+
+
+	r = rand() % 4;
+
+
+
+	for (i = 0; i < 12; i++)
+
+	{
+
+		if (r == 1)
+
+		{
+
+			password[i] = numbers[rand() % 10];
+
+			r = rand() % 4;
+
+			printf("%c", password[i]);
+
+		}
+
+		else if (r == 2)
+
+		{
+
+			password[i] = symbols[rand() % 8];
+
+			r = rand() % 4;
+
+			printf("%c", password[i]);
+
+		}
+
+		else if (r == 3)
+
+		{
+
+			password[i] = LETTER[rand() % 26];
+
+			r = rand() % 4;
+
+			printf("%c", password[i]);
+
+		}
+
+		else
+
+		{
+
+			password[i] = letter[rand() % 26];
+
+			r = rand() % 4;
+
+			printf("%c", password[i]);
+
+		}
+
+	}
+
+	return (0);
+
+}
+
+
+
+
+
+
+
+int main(void)
+
+{
+
+	key_gen();
+
+
+
+	return (0);
+
+}
